2c70ea96eb80560805e080c87ab54cbe6c54cb5f
Taha <94710769+alferkh@users.noreply.github.com>
2021-12-21 15:55:30 +0100

    Update server.sh

--- server.sh
+++ server.sh
@@ -4,26 +4,26 @@ declare -r FIFO=fifo
 declare -r PORT=$1
 
 if ! [[ $# = 1 && $PORT =~ ^[0-9]+$ ]]; then
-echo "You have entered an invalid port, please choose another one"
-exit 1
+  echo "You have entered an invalid port, please choose another one"
+  exit 1
 fi
 
 #nettoyage
 function clean() {
-rm -f fifo;
+  rm -f fifo;
 }
 trap clean EXIT
 
 function interaction() {
-local mode args
-while true; do
-read mode args || exit -1
-bash vsh_server.sh $mode $args
-exit $?
+  local mode args
+  while true; do
+  read mode args || exit -1
+  bash vsh_server.sh $mode $args
+  exit $?
 done
 }
 
 echo "Port $PORT is up and running ..."
 while true; do
-interaction < "FIFO" | netcat -l -k $PORT > "$FIFO"
+  interaction < "FIFO" | netcat -l -k $PORT > "$FIFO"
 done

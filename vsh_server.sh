e0f6f35623e811415d5349464aeb4fc04a876416
Taha <94710769+alferkh@users.noreply.github.com>
2021-12-21 15:59:37 +0100

    Update vsh_server.sh

--- vsh_server.sh
+++ vsh_server.sh
@@ -3,46 +3,33 @@ declare -r args=$2
 declare -r archives="archives" ##archive by default
 
 function archive_existe() {
-if [[ -f "archives/$1" ]]; then
-return 0
-else
-return 1
-fi
+   if [[ -f "archives/$1" ]]; then
+     return 0
+   else
+     return 1
+   fi
 }
 
 fucntion check_archive() {
-if ! archive_existe $1; then
-echo "Archive '$1' does not exist, please enter another archive"
-exit 1
-fi
+  if ! archive_existe $1; then
+    echo "Archive '$1' does not exist, please enter another archive"
+  exit 1
+  fi
 }
 
 function list() {
-tree $archives
+  tree $archives
 }
 
 function browse() {
-bash browse.sh $1
+  bash browse.sh $1
 }
 
-function extract() {
-
-}
-
-if [ $mode == "list" ]; then
-list
-elif [ $mode == "extract" ]; then
-arch=$(echo $args | cut -d ' ' -f1)
-if ! archive_exists $arch; then
-echo -e "\nVSH_EXTRACT_UNK_ARCH"
-echo -e "\nno archive named $arch"
-exit 1
-fi
-cat "$archive_dir/$arch"
-echo -e "\nVSH_END_EXTRACT"
-elif [ $mode == "browse" ]; then
-arch=$(echo $args | cut -d ' ' -f1)
-browse $arch
-elif [ $mode == "exit" ]; then
-exit 0
+  if [ $mode == "list" ]; then
+    list
+  elif [ $mode == "browse" ]; then
+    arch=$(echo $args | cut -d ' ' -f1)
+    browse $arch
+  elif [ $mode == "exit" ]; then
+    exit 0
 fi
